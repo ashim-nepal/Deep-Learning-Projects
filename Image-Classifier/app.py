0aa706a28f5648426b647701d9b4d008dfc988e2
ashim-nepal <npashim015@gmail.com>
2024-10-20 12:23:06 +0545

    image classifier app!

---
+++ Image-Classifier/app.py
@@ -0,0 +1,70 @@
+import streamlit as st
+import numpy as np
+from PIL import Image
+import tensorflow as tf
+print(tf.__version__)
+
+# Load the trained model
+model = tf.keras.models.load_model('image_classifier_model.h5')
+
+# Dictionary to map the predicted classes to their labels with emojis
+results = {
+    0: '✈️ Aeroplane',
+    1: '🚗 Automobile',
+    2: '🐦 Bird',
+    3: '🐱 Cat',
+    4: '🦌 Deer',
+    5: '🐶 Dog',
+    6: '🐸 Frog',
+    7: '🐴 Horse',
+    8: '🚢 Ship',
+    9: '🚚 Truck'
+}
+
+# Streamlit app title
+st.title('Image Classification App')
+
+# Display the list of classes with emojis
+st.subheader('List of Classes that can be Recognized:')
+st.markdown(
+    """
+    - ✈️ **Aeroplane**
+    - 🚗 **Automobile**
+    - 🐦 **Bird**
+    - 🐱 **Cat**
+    - 🦌 **Deer**
+    - 🐶 **Dog**
+    - 🐸 **Frog**
+    - 🐴 **Horse**
+    - 🚢 **Ship**
+    - 🚚 **Truck**
+    """,
+    unsafe_allow_html=True,
+)
+
+# Upload image
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+
+if uploaded_file is not None:
+    # Open the image
+    image = Image.open(uploaded_file)
+
+    # Display the uploaded image
+    st.image(image, caption='Uploaded Image.', use_column_width=True)
+    st.write("")
+
+    # Preprocess the image
+    image = image.resize((32, 32))
+    image = image.convert('RGB')
+    image_array = np.array(image)
+    image_array = image_array.astype('float32') / 255.0
+    image_array = np.expand_dims(image_array, axis=0)
+
+    # Make a prediction
+    pred = model.predict(image_array)
+    pred_class = np.argmax(pred, axis=1)[0]
+    confidence_score = pred[0][pred_class] * 100
+
+    # Display the prediction
+    st.write(f"Predicted class: {results[pred_class]}")
+    st.write(f"Confidence: {confidence_score:.2f}%")
